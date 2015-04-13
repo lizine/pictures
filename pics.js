69105e7d3624f5d4a9b5e7354c7ff9ea14aca7e2
Nini <nini@localhost.localdomain>
2015-04-13 20:32:59 +0300

    new title, added some comments to js-file

--- pics.js
+++ pics.js
@@ -1,9 +1,7 @@
 
-//var ul = document.getElementById("kissalista");
-//var kuvat = ["img/cat1.jpg","img/cat2.jpg","https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRYXNLuN8M8-f0TZUM9DIiMD3bNN6B8hyvlyttFrUdN423bn7ZD","https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQuwWbUXC-lgzQHp-j1iw56PIgl_2eALrEENUP-ld72gq3s8cVo","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKOFCn9IQhAS3zI4AQcHNJTZ_VuDX03JI0p2IiD_APV_98HD6SUA", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNR4FsHFORXZ0Wc4P5d49U-twNUN5v0qN6j1V280BcysPkeicb"];
-//var tekstit = ["First kittens text 1","Second kittens text 2","Third kittens text 3","Fourth kittens text 4","Fifth kittens text 5","Sixth kittens text 6"];
 
-// lets put json data into cats, variable
+
+// lets put json data into cats variable so we can easily iterate trough it
 var cats = {images: [{url : "img/cat1.jpg", text : "kitten text 1"}, {url : "img/cat2.jpg", text: "kitten text 2"},
 {url: "img/cat3.jpg", text:"kitten text 3"}, {url: "img/cat4.jpg", text:"kitten text 4"},{url: "img/cat5.jpg", text:"kitten text 5"},{url: "img/cat6.jpg", text:"kitten text 6"} ]}
 

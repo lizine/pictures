e47eb5e760c438c8e69655d4b0fff7c85a218544
Nini <nini@localhost.localdomain>
2015-04-14 13:25:40 +0300

    stored fetched dom elements in their own variables

--- pics.js
+++ pics.js
@@ -1,21 +1,28 @@
 
+// store dom elements in variables, so we don't need to fetch them repeatedly.
+var mainpic = $("#mainpic" );
+var text = $("#text" );
+var pictures = $("#pictures");
 
 
 // lets put json data into cats variable so we can easily iterate trough it
 var cats = {images: [{url : "img/cat1.jpg", text : "Kitten text 1"}, {url : "img/cat2.jpg", text: "Kitten text 2"},
 {url: "img/cat3.jpg", text:"Kitten text 3"}, {url: "img/cat4.jpg", text:"Kitten text 4"},{url: "img/cat5.jpg", text:"Kitten text 5"},{url: "img/cat6.jpg", text:"Kitten text 6"} ]}
 
+
+
 // initial cat picture, cat number 0
-$("#mainpic" ).append('<img src="' + cats.images[0].url + '"/>');
-$("#text" ).append(cats.images[0].text);
+mainpic.append('<img src="' + cats.images[0].url + '"/>');
+text.append(cats.images[0].text);
 
 // iterating trought cats, adding new img-element with cats picture and appendig those elements to pictures-div
-for(var i=0; i<6; i++){
+for(var i=0; i<cats.images.length; i++){
 	
- $("#pictures").append('<img class="thumb" id="' + i + '" src="' + cats.images[i].url + '"/>');
- if((i+1)%3==0){
- 	$("#pictures").append('<br/>');
- }
+   pictures.append('<img class="thumb" id="' + i + '" src="' + cats.images[i].url + '"/>');
+
+      if((i+1)%3==0){
+ 	    pictures.append('<br/>');
+      }
 
 
 }
@@ -36,9 +43,9 @@ pics.onclick = function(event) {
     	return;
     }
 
-    $("#mainpic" ).empty();
-    $("#mainpic" ).append('<img src="' + cats.images[target.id].url + '"/>');
-    $("#text" ).empty();
-    $("#text" ).append(cats.images[target.id].text);
+    mainpic.empty();
+    mainpic.append('<img src="' + cats.images[target.id].url + '"/>');
+    text.empty();
+    text.append(cats.images[target.id].text);
  
 };
